5124ea9d598231237aedef6a271fb3eecd9fda80
viniceosm <miiller.vini@gmail.com>
2018-08-06 18:31:21 -0300

    First commmit

---
+++ autocompletev.js
@@ -0,0 +1,50 @@
+function Autocompletev(){}
+
+Autocompletev.prototype.adicionaEventosDataList = function(selector) {
+	var elemento = document.querySelector('.datalist-input' + selector);
+
+	elemento.addEventListener('input', function (e) {
+		filtrarAutoComplete(elemento.value, elemento.getAttribute('target'));
+	});
+	
+	elemento.addEventListener('focus', function (e) {
+		mostraDatalist(elemento.getAttribute('target'));
+	});
+
+	elemento.addEventListener('blur', function (e) {
+		setTimeout(function() {
+ 			escondeDatalist(elemento.getAttribute('target'));
+		}, 250);
+	});
+	
+	var elTarget = elemento.getAttribute('target');
+	var options = document.querySelectorAll(elTarget + ' div');
+	
+	[].forEach.call(options, function (opt) {
+		opt.addEventListener('click', function (e) {
+			elemento.value = opt.innerHTML;
+		});
+	});
+	
+	function mostraDatalist(elTarget) {
+		document.querySelector(elTarget).style.display = 'block';
+	}
+
+	function escondeDatalist(elTarget) {
+		document.querySelector(elTarget).style.display = 'none';
+	}
+	
+	function filtrarAutoComplete(valor, elTarget) {
+		var options = document.querySelectorAll(elTarget + ' div');
+
+		[].forEach.call(options, function (opt) {
+			if (opt.innerHTML.indexOf(valor) == -1) {
+				opt.style.display = 'none';
+			} else {
+				opt.style.display = '';
+			}
+		});
+	}
+}
+
+var autocompletev = new Autocompletev();
