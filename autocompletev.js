20004ecd70cd52bc21e91994d5b042f63e204f56
viniceosm <miiller.vini@gmail.com>
2018-08-07 09:21:11 -0300

    Corrigido problema de blur sem usar timeout

--- autocompletev.js
+++ autocompletev.js
@@ -2,30 +2,46 @@ function Autocompletev(){}
 
 Autocompletev.prototype.adicionaEventosDataList = function(selector) {
 	var elemento = document.querySelector('.datalist-input' + selector);
+	var clicouOp = false;
+	var deuBlurInput = false;
 
 	elemento.addEventListener('input', function (e) {
 		filtrarAutoComplete(elemento.value, elemento.getAttribute('target'));
 	});
-	
+
 	elemento.addEventListener('focus', function (e) {
 		mostraDatalist(elemento.getAttribute('target'));
 	});
 
 	elemento.addEventListener('blur', function (e) {
-		setTimeout(function() {
- 			escondeDatalist(elemento.getAttribute('target'));
-		}, 250);
+		deuBlurInput = true;
+
+		if (!clicouOp) {
+			escondeDatalist(elemento.getAttribute('target'));
+		}
 	});
-	
+
+	document.addEventListener('mousedown', function (event) {
+		if (/\bdatalist-option\b/.test(event.target.className)) {
+			clicouOp = true;
+		}
+	});
+
 	var elTarget = elemento.getAttribute('target');
 	var options = document.querySelectorAll(elTarget + ' div');
-	
+
 	[].forEach.call(options, function (opt) {
 		opt.addEventListener('click', function (e) {
 			elemento.value = opt.innerHTML;
+			clicouOp = false;
+
+			if (deuBlurInput) {
+				escondeDatalist(elemento.getAttribute('target'));
+				deuBlurInput = false;
+			}
 		});
 	});
-	
+
 	function mostraDatalist(elTarget) {
 		document.querySelector(elTarget).style.display = 'block';
 	}
@@ -33,7 +49,7 @@ Autocompletev.prototype.adicionaEventosDataList = function(selector) {
 	function escondeDatalist(elTarget) {
 		document.querySelector(elTarget).style.display = 'none';
 	}
-	
+
 	function filtrarAutoComplete(valor, elTarget) {
 		var options = document.querySelectorAll(elTarget + ' div');
 
